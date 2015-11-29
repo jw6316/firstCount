a33ea38f578beb19311632e01bcc477ec254595e
Jamie Walker <jw6316@gmail.com>
2015-11-30 02:52:54 +0900

    Basis for something potentially larger

--- first swift holyshit/ViewController.swift
+++ first swift holyshit/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.fr
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    @IBOutlet var label: UILabel!
+    var number = 0
+    
+    @IBAction func PURASU(){
+        number++
+        label.text = String(number)
+        
+        if number >= 60 && number < 120{
+            label.textColor = UIColor.blueColor()
+        }else if number >= 120 && number < 180{
+            label.textColor = UIColor.brownColor()
+        }else if number == 180 {
+            label.textColor = UIColor.redColor()
+        }
+    }
+    
+    @IBAction func CLEAR(){
+        number = 0
+        label.text = String(number)
+    }
+    
+    //for working out, change colour every 60 
 
 }
-
